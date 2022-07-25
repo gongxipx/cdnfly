44c1cf8ca97bf779bd7960ee2e1f53545b235113
gongxipx <68416666+gongxipx@users.noreply.github.com>
2022-07-26 01:19:11 +0800

    Update install.sh

--- cdn/install.sh
+++ cdn/install.sh
@@ -27,16 +27,15 @@ firewall-cmd --zone=public --add-port=80/tcp --permanent
 firewall-cmd --zone=public --add-port=443/tcp --permanent
 firewall-cmd --reload
 
+#安装php7.4
 yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm 
-yum install -y php74-php-fpm php74-php-cli php74-php-bcmath php74-php-gd php74-php-json  php74-php-mbstring php74-php-mcrypt php74-php-mysqlnd php74-php-opcache php74-php-pdo php74-php-pecl-crypto php74-php-pecl-mcrypt php74-php-pecl-geoip php74-php-recode php74-php-snmp php74-php-soap php74-php-xml php74-php-imagick php74-php-pecl-zip
-
+yum install -y php74-php php74-php-fpm
 systemctl start php74-php-fpm
 #4.添加开机启动
 systemctl enable php74-php-fpm
 #5.查看版本
 cp /usr/bin/php74 /usr/bin/php #如果不复制的话，所有的php 替换成php74
 chmod +x /usr/bin/php #文件给与执行权限
-php -v
 
 groupadd -r www
 useradd -r -g www www
@@ -44,8 +43,8 @@ useradd -r -g www www
 sed -i "s/user = apache/user = www/g" /etc/php-fpm.d/www.conf
 sed -i "s/group = apache/group = www/g" /etc/php-fpm.d/www.conf
 
-systemctl start php-fpm
-systemctl enable php-fpm
+systemctl start php74-php-fpm
+systemctl enable php74-php-fpm
 
 mkdir /etc/nginx/vhost
 echo '
